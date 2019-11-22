e616be014e12f2d29e0466963a1ac3b765ae3084
Bruno Arthur <brunoartc@yahoo.com>
2019-11-22 20:56:38 -0300

    check openvpn installation & start connection

--- create-conection.sh
+++ create-conection.sh
@@ -11,5 +11,5 @@ sudo cp /var/lib/cloud/instances/*/user-data.txt /home/ubuntu/user-data.txt
 echo ' ' >> /home/ubuntu/user-data.txt
 echo 'echo "' >> /home/ubuntu/user-data.txt
 cat /home/ubuntu/openvpn_client.conf >> /home/ubuntu/user-data.txt
-echo '" >> openvpn.config' >> /home/ubuntu/user-data.txt
+echo '" >> /home/ubuntu/openvpn.config' >> /home/ubuntu/user-data.txt
 sudo tmux new -d -s vpn 'openvpn --config /home/ubuntu/openvpn.conf;'
